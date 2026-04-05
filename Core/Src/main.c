f2b8ea7ba7b9111c0f978abdfe905997145a9fe7
Jacob Nguyen <jacobnguyen.dev@gmail.com>
2026-04-05 16:25:30 -0400

    Revamp main to abstract peripheral initialization

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,27 +23,34 @@ volatile bool disable_dma = false;
 volatile bool isr_tim6 = false;
 volatile bool isr_tim7 = false;
 
-int main(void) {
-    { // config
-    Debug_Init();
-    // Enable IO clock
+static void MX_GPIO_Init(void) {
+    // Enable IO clock for all active ports
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
 
-    GPIO_SetMode(GPIOC, 13, GPIO_MODE_INPUT); // On-board USER pushbutton
+    // Configure PC13 as On-board USER pushbutton with Pull-Up
+    GPIO_SetMode(GPIOC, 13, GPIO_MODE_INPUT);
     GPIO_SetPull(GPIOC, 13, GPIO_PULL_UP);
 
-    GPIO_SetMode(GPIOA, 5, GPIO_MODE_OUTPUT); // On-board LED
+    // Configure PA5 as On-board LED
+    GPIO_SetMode(GPIOA, 5, GPIO_MODE_OUTPUT);
 
-    GPIO_SetMode(GPIOB, 6, GPIO_MODE_AF); // IN1 (TIM4_CH1) - Left
-    GPIO_SetMode(GPIOB, 7, GPIO_MODE_AF); // IN2 (TIM4_CH2) - Left
-    GPIO_SetMode(GPIOB, 8, GPIO_MODE_AF); // IN3 (TIM4_CH3) - Right
-    GPIO_SetMode(GPIOB, 9, GPIO_MODE_AF); // IN4 (TIM4_CH4) - Right
+    // Configure PA6 for IR Receiver Input
+    GPIO_SetMode(GPIOA, 6, GPIO_MODE_AF);
+    GPIO_SetAF(GPIOA, 6, 2);
 
+    // Configure PB6, PB7, PB8, PB9 for Motor PWM (TIM4)
+    GPIO_SetMode(GPIOB, 6, GPIO_MODE_AF);
+    GPIO_SetMode(GPIOB, 7, GPIO_MODE_AF);
+    GPIO_SetMode(GPIOB, 8, GPIO_MODE_AF);
+    GPIO_SetMode(GPIOB, 9, GPIO_MODE_AF);
     GPIO_SetAF(GPIOB, 6, 2);
     GPIO_SetAF(GPIOB, 7, 2);
     GPIO_SetAF(GPIOB, 8, 2);
     GPIO_SetAF(GPIOB, 9, 2);
+}
 
+static void MX_TIM4_Init(void) {
+    // Enable the Motor PWM Timer (TIM4)
     RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 
     PWM_InitTypeDef pwm_init = { 
@@ -56,34 +63,55 @@ int main(void) {
     PWM_Init(TIM4, 2, &pwm_init);
     PWM_Init(TIM4, 3, &pwm_init);
     PWM_Init(TIM4, 4, &pwm_init);
+}
 
-    // input capture timer 
+static void MX_TIM3_Init(void) {
+    // IR Receiver Input Capture timer (1MHz counting)
     TIM_InitTypeDef tim3_init = { .Prescaler = (TIM_GetClock(TIM3) / 1000000) - 1, .Period = 65535 };
     TIM_Init(TIM3, &tim3_init);
+    
+    // Capture both edges to timestamp IR pulses
     TIM_InputCapture_Init(TIM3, 1, TIM_BOTH_EDGES);
     TIM_InputCapture_Init(TIM3, 2, TIM_BOTH_EDGES);
-    TIM3->DIER |= TIM_DIER_CC2IE; // used for timeout
+    
+    // Enable timer interrupts for timeouts
+    TIM3->DIER |= TIM_DIER_CC2IE;
     NVIC_EnableIRQ(TIM3_IRQn);
     TIM_Start(TIM3);
+}
 
-    // connect to PIN_A6 to IR receiver
-    GPIO_SetMode(GPIOA, 6, GPIO_MODE_AF);
-    GPIO_SetAF(GPIOA, 6, 2);
-
-    DMA_TIM3_CH1_Init(timestamps, BUF_SIZE);
-
-    // timeout for decoding instruction
-    TIM_InitTypeDef tim6_init = { .Prescaler = (TIM_GetClock(TIM6) / 1000000) - 1, .Period = 10000 }; // 10ms
+static void MX_TIM6_Init(void) {
+    // Timeout timer for decoding instruction (10ms)
+    TIM_InitTypeDef tim6_init = { .Prescaler = (TIM_GetClock(TIM6) / 1000000) - 1, .Period = 10000 };
     TIM_Init(TIM6, &tim6_init);
     TIM6->DIER |= TIM_DIER_UIE;
     NVIC_EnableIRQ(TIM6_DAC_IRQn);
+}
 
-    // timeout for repeat instructions
-    TIM_InitTypeDef tim7_init = { .Prescaler = (TIM_GetClock(TIM7) / 100000) - 1, .Period = 20000 }; // 200ms
+static void MX_TIM7_Init(void) {
+    // Timeout timer for repeat instructions (200ms)
+    TIM_InitTypeDef tim7_init = { .Prescaler = (TIM_GetClock(TIM7) / 100000) - 1, .Period = 20000 };
     TIM_Init(TIM7, &tim7_init);
     TIM7->DIER |= TIM_DIER_UIE;
     NVIC_EnableIRQ(TIM7_IRQn);
-    }
+}
+
+static void MX_DMA_Init(void) {
+    // Initialize DMA strictly for TIM3 CH1 transfers
+    DMA_TIM3_CH1_Init(timestamps, BUF_SIZE);
+}
+
+int main(void) {
+    // Initial hardware clock/debug setup
+    Debug_Init();
+    
+    // Core Peripheral Initialization
+    MX_GPIO_Init();
+    MX_TIM4_Init();
+    MX_TIM3_Init();
+    MX_DMA_Init();
+    MX_TIM6_Init();
+    MX_TIM7_Init();
     while(1) {
         if (dma_complete) {
             uint8_t comm = IR_Decode(timestamps, BUF_SIZE);
